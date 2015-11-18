f7e4a145724608b3005fc6dda6c8213f309a094d
Christian Herz <christian.herz@mevis.fraunhofer.de>
2015-11-17 23:26:14 -0500

    ENH: changed code in order to be able to use at with DICOMSearch - removed search - replaced button by input field which is not editable but clickable

--- src/wan-select.js
+++ src/wan-select.js
@@ -1,3 +1,5 @@
+// original sourcecode: https://github.com/feichao/wan-select licensed under MIT
+
 (function() {
   'use strict';
 
@@ -40,34 +42,30 @@
    */
   function WanSelect() {
 
-    var template = ['<div class="wan-select" outside-click="vm.hideSelect()">',
-      '  <label>{{placeholder}}</label>',
-      '  <md-button class="md-raised" ng-click="vm.showSelect()">{{vm.result}}</md-button>',
+    var template = [
+      '<div class="wan-select" outside-click="vm.hideSelect()">',
+      '  <label class="md-static">{{placeholder}}</label>',
+      '  <div layout="row" layout-wrap>',
+      '    <div flex="50" ng-click="vm.showSelect()">',
+      '      <input ng-readonly="true" value="{{vm.result}}" >',
+      '    </div>',
+      '    <div flex="5" class="showHand" ng-click="vm.showSelect()">',
+      '      <ng-md-icon aria-label="Search DICOM Standard" icon="arrow_drop_down" size="32"></ng-md-icon>',
+      '    </div>',
+      '  </div>',
       '  <div class="ws-content" ng-class="{true: \'ws-content-show\', false: \'ws-content-hide\'}[vm.show]">',
       '    <div class="ws-select-shortcut">',
-      '      <md-button class="md-raised ws-select-button" ng-click="vm.selectAll()">全选</md-button>',
-      '      <md-button class="md-raised ws-select-button" ng-click="vm.selectNone()">全不选</md-button>',
-      '      <md-button class="md-raised ws-select-button" ng-click="vm.selectToggle()">反选</md-button>',
-      '      <div class="ws-clear"></div>',
-      '    </div>',
-      '    <hr>',
-      '    <md-input-container md-no-float class="ws-search">',
-      '      <md-button class="ws-clear-search" ng-click="vm.searchStr=\'\'">',
-      '        清空',
-      '      </md-button>',
-      '      <input ng-model="vm.searchStr" class="ws-search-input" placeholder="搜索">',
-      '    </md-input-container>',
-      '    <div layout="row" layout-wrap class="ws-selected">',
-      '      <span ng-show="selectedData.length === 0" class="ws-none-select">没有选择任何项</span>',
-      '      <md-checkbox ng-checked="true" ng-click="vm.toggle($event, item, true)" ng-repeat="item in selectedData">',
-      '        {{item}}',
-      '      </md-checkbox>',
+      '     <div layout="row" layout-wrap>',
+      '       <div flex="100">',
+      '         <md-button class="md-raised ws-select-button" ng-click="vm.selectAll()">select all</md-button>',
+      '         <md-button class="md-raised ws-select-button" ng-click="vm.selectNone()">select none</md-button>',
+      '       </div>',
+      '     </div>',
       '    </div>',
       '    <hr>',
       '    <div layout="row" class="ws-unselected" layout-wrap>',
-      '      <span ng-show="vm.dataUnselected.length === 0" class="ws-none-select">没有项可供选择</span>',
-      '      <md-checkbox ng-checked="false" ng-click="vm.toggle($event, item, false)" ng-repeat="item in vm.dataUnselected">',
-      '        {{item}}',
+      '      <md-checkbox ng-checked="vm.isSelected(item)" ng-click="vm.toggle($event, item)" ng-repeat="item in vm.sourceData">',
+      '        {{item.part}}:{{item.name}}',
       '      </md-checkbox>',
       '    </div>',
       '  </div>',
@@ -79,8 +77,7 @@
       scope: {
         sourceData: '=',
         selectedData: '=',
-        placeholder: '@',
-        selectChanged: '&'
+        placeholder: '@'
       },
       template: template,
       controller: Controller,
@@ -95,7 +92,7 @@
 
       vm.result = '请选择';
 
-      vm.dataUnselected = $scope.sourceData;
+      vm.sourceData = $scope.sourceData
       vm.searchStr = '';
 
       $scope.$watch('vm.searchStr', function(newVal, oldVal) {
@@ -104,19 +101,12 @@
 
       $scope.$watchCollection('selectedData', function(newVal, oldVal) {
         vm.setSearchResult();
-        if($scope.selectChanged && typeof($scope.selectChanged) === 'function'){
-          $scope.selectChanged();
-        }
       });
 
       function search(val) {
         val = val.toUpperCase();
         var tempSelected = $scope.selectedData.map(function(dt) {
           return dt.toUpperCase();
-        });
-        vm.dataUnselected = $scope.sourceData.filter(function(dt) {
-          dt = dt.toUpperCase();
-          return dt.indexOf(val) > -1 && tempSelected.indexOf(dt) === -1;
         });
       }
 
@@ -130,60 +120,52 @@
           return;
         }
         vm.show = true;
+        //vm.animate({scrollTop: 0}, "slow");
         vm.searchStr = '';
-
-        vm.dataUnselected = $scope.sourceData.filter(function(dt) {
-          return $scope.selectedData.indexOf(dt) === -1;
-        });
       };
 
-      vm.toggle = function(event, item, isExist) {
+      vm.toggle = function(event, item) {
         event.stopPropagation();
-
-        var idx;
-        if (isExist) {
-          idx = $scope.selectedData.indexOf(item);
-          $scope.selectedData.splice(idx, 1);
-          vm.dataUnselected.push(item);
-        } else {
-          idx = vm.dataUnselected.indexOf(item);
-          vm.dataUnselected.splice(idx, 1);
+        var idx = $scope.selectedData.indexOf(item);
+        if (idx == -1) {
+          console.log("adding part")
           $scope.selectedData.push(item);
+        } else {
+          console.log("removing part")
+          $scope.selectedData.splice(idx, 1);
         }
-
         vm.setSearchResult();
+      };
+
+      vm.isSelected = function(item) {
+        return $scope.selectedData.indexOf(item) != -1;
       };
 
       vm.setSearchResult = function() {
         var length = $scope.selectedData.length;
-        if (length === 1) {
-          vm.result = $scope.selectedData[0];
-        } else if (length === 0) {
-          vm.result = '请选择';
+        if (length === 0) {
+          vm.result = 'None';
         } else if (length === $scope.sourceData.length) {
-          vm.result = '选择所有';
+          vm.result = 'All Parts';
+        } else if (length > 3) {
+          vm.result = length.toString() + " Parts selected" ;
         } else {
-          vm.result = '选择多个';
+          vm.result = ""
+          angular.forEach($scope.selectedData, function(value, key){
+            vm.result += value.part + " ";
+          });
         }
       };
 
       vm.selectAll = function() {
-        vm.result = '选择所有';
+        vm.result = 'All Parts';
         $scope.selectedData = [].concat($scope.sourceData);
-        vm.dataUnselected = [];
+        //vm.dataUnselected = [];
       };
 
       vm.selectNone = function() {
-        vm.result = '请选择';
-        vm.dataUnselected = [].concat($scope.sourceData);
+        vm.result = 'None';
         $scope.selectedData = [];
-      };
-
-      vm.selectToggle = function() {
-        var temp = [].concat(vm.dataUnselected);
-        vm.dataUnselected = [].concat($scope.selectedData);
-        $scope.selectedData = temp;
-        vm.setSearchResult();
       };
     }
   }
